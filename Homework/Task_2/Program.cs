92179074f300490e3fd3447c185a840c86ed22d3
Artem <remenusei@yandex.ru>
2022-08-07 01:07:22 +0700

    Homework  Done

---
+++ Homework/Task_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите число - ");
+int n= Convert.ToInt32(Console.ReadLine());
+int rez=0;
+int n1=n;
+while (n1>1) {
+    rez=rez+n1%10;
+    n1=n1/10;
+}
+Console.WriteLine(rez);
